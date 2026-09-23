d461765ab5dd6023c19701773a2632adfc5f6046
SeaNPPNJX <140157222+SeaNPPNJX@users.noreply.github.com>
2026-09-23 18:57:41 +0700

    finished miniproject

---
+++ rush/ex00/checkmate.py
@@ -0,0 +1,39 @@
+from pieces import *
+
+def oversize(pos=set, size=int):
+    return set([i for i in pos if i[0] >= 0 and i[1] >= 0 and i[0] < size and i[1] < size])
+
+def pawn(px, py, size):
+    return oversize([(px-1, py-1), (px+1, py-1)], size)
+
+def bishop(px, py, size):
+    pos = []
+    for i in range(1, size):
+        pos.extend([(px-i,py-i),(px+i,py+i),(px-i,py+i),(px+i,py-i)])
+    return oversize(set(pos), size)
+
+def rook(px, py, size):
+    pos = []
+    for i in range(1, size):
+        pos.extend([(px,py-i),(px-i,py),(px,py+i),(px+i,py)])
+    return oversize(set(pos), size)
+
+def queen(px, py, size):
+    return rook(px, py, size) | bishop(px,py, size)
+
+def checkmate(board):
+    matrix = [list(i) for i in board.splitlines()]
+    size = len(matrix)
+    attacks = set()
+    for y, row in enumerate(matrix):
+        for x, value in enumerate(row):
+            if value == "R":
+                attacks |= rook(x, y, size)
+            if value == "P": 
+                attacks |= pawn(x, y, size)
+            if value == "Q":
+                attacks |= queen(x, y, size)
+            if value == "K":
+                king_pos = (x, y)
+    print("Success" if king_pos in attacks else "Fail")
+
---
+++ rush/ex00/main.py
@@ -0,0 +1,19 @@
+from checkmate import *
+
+def main():
+    board = """\
+R...
+.K..
+..P.
+....\
+"""
+
+# board = """\
+# ..
+# .K\
+# """
+
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
