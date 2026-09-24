900e2e2c86b0fa92d478d7ba7616e3873c38dcb5
SeaNPPNJX <140157222+SeaNPPNJX@users.noreply.github.com>
2026-09-24 15:18:31 +0700

    update ex01

--- rush/ex01/checkmate.py
+++ rush/ex01/checkmate.py
@@ -1,28 +1,62 @@
 from check_invalid_board import *
 
+PIECES ={"R","Q","P","B"}
+
+def line_attack(px, py, size, directions, board):
+    pos = []
+    for dx, dy in directions:
+        for i in range(1, size):
+            x = px + dx * i
+            y = py + dy * i
+
+            if not (0 <= x < size and 0 <= y < size):
+                break
+
+            pos.append((x, y))
+
+            if board[y][x] in PIECES:
+                break
+
+    return set(pos)
+#. . 2 . .
+#. . 1 . .      (0, 1)
+#2 1 R 1 2 (-1, 0) R (1, 0)
+#. . 1 . .      (0, -1)
+#. . 2 . .
+
+#2 . . . 2
+#. 1 . 1 . (-1, -1) (1, 1)
+#. . B . .         B
+#. 1 . 1 . (-1, 1)  (1, -1)
+#2 . . . 2
 def pawn(px: int, py: int):
     return {(px-1, py-1), (px+1, py-1)}
+# X . X
+# . P .
+# . . .
 
-def bishop(px: int, py: int, size: int):
-    pos = []
-    for i in range(1, size):
-        pos.extend([(px-i,py-i),(px+i,py+i),(px-i,py+i),(px+i,py-i)])
-    return set(pos)
 
-def rook(px: int, py: int, size: int):
-    pos = []
-    for i in range(1, size):
-        pos.extend([(px,py-i),(px-i,py),(px,py+i),(px+i,py)])
-    return set(pos)
+def rook(px, py, size, board):
+    return line_attack(
+        px, py, size,
+        [(0, -1), (-1, 0), (0, 1), (1, 0)],
+        board
+    )
 
-def queen(px: int, py: int, size: int):
-    return rook(px, py, size) | bishop(px,py, size)
+def bishop(px, py, size, board):
+    return line_attack(
+        px, py, size,
+        [(-1, -1), (1, 1), (-1, 1), (1, -1)],
+        board
+    )
+
+def queen(px, py, size, board):
+    return rook(px, py, size, board) | bishop(px, py, size, board)
 
 def checkmate(board: str):
     invalid, error_message = check_invalid_board(board)
     if invalid:
         return print(error_message)
-
     matrix = [list(i.strip()) for i in board.splitlines()]
     size = len(matrix)
     attacks = set()
@@ -31,20 +65,15 @@ def checkmate(board: str):
         #1 ['.', 'K', '.', '.']
         #2 ['.', '.', 'P', '.']
         #3 ['.', '.', '.', '.']
-        print(y, row)
         for x, value in enumerate(row):
-            #0 R, 1 ., 2 . ,3 .
-            print(x, value)
             if value == "R":
-                attacks |= rook(x, y, size)
-            if value == "P": 
+                attacks |= rook(x, y, size, matrix)
+            if value == "P":
                 attacks |= pawn(x, y)
             if value == "B":
-                attacks |= bishop(x, y, size)
+                attacks |= bishop(x, y, size, matrix)
             if value == "Q":
-                attacks |= queen(x, y, size)
+                attacks |= queen(x, y, size, matrix)
             if value == "K":
                 king_pos = (x, y)
-        print(attacks)
     print("Success" if king_pos in attacks else "Fail")
-
